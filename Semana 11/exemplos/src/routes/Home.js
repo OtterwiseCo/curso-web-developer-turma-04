2bb1c5fdde474c0eacf1b1d661b739d2f7284de8
João Pedro Bretanha <joaopedro@nave.rs>
2022-01-17 22:02:46 -0300

    adicionando exemplo local storage

--- Semana 11/exemplos/src/routes/Home.js
+++ Semana 11/exemplos/src/routes/Home.js
@@ -1,11 +1,22 @@
 import { Link } from "react-router-dom";
 
 const Home = () => {
+  const login = () => {
+    localStorage.setItem(
+      "token",
+      "aioshdiahuid1u38918ghd9as89d8918903281h89ia",
+    );
+  };
+
+  console.log(localStorage.getItem("token"));
+
   return (
     <>
       <Link to="/users">Users</Link>
       <br />
       <Link to="/posts">Posts</Link>
+      <br />
+      <button onClick={login}>Me logar</button>
     </>
   );
 };
